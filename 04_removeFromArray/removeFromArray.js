008fdf2458722ad5fb71f5e03200ac3d0f1cce0e
Ciber <cibermonster@gmail.com>
2022-07-13 15:09:44 -0400

    Cleanup

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,12 @@
 
 
 const removeFromArray = function() {
-    console.table(arguments)
     var arr = arguments[0]
     var remarr = arguments
     const len = arguments.length
-    console.log("Before: "+arr)
-    console.log("toRemove: "+len)
     
     for (i = 0; i <= len+1; i++) {
         if (arr.indexOf(arguments[i]) != '-1') {
-            console.log("Removed: "+arguments[i])
             arr.splice(arr.indexOf(arguments[i]), 1)
         }
     }
