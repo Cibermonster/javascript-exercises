642e673fe6c6763b5e0b962aa471b156232698e3
Ciber <cibermonster@gmail.com>
2022-07-13 13:29:44 -0400

    Removed 1 var

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,20 @@
+
+
 const removeFromArray = function() {
     var arr = arguments[0]
+    const len = arr.length
     console.log("Before: "+arr)
-    for (i = arguments.length - 1; i > 0; i--) {
-        
+    
+    /*
+    for (i = 0; i < len; i++) {  
+        console.log("Count: "+i)
+        arr.splice(i, 1)
     }
-    return arr
+    console.log("After: "+arr)
+    return arr*/
+    arr.splice(arr.indexOf(arguments[1]), 1)
+    console.log("After: "+arr)
+    return arr;
 };
 
 // Do not edit below this line
