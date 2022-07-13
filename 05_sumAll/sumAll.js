c9e662f99fd85b327004aac8f9e76491fe130c12
Ciber <cibermonster@gmail.com>
2022-07-13 15:33:26 -0400

    Passed #1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
 const sumAll = function(x, y) {
-
+    var sum = x
+    for (i = x; i < y; i++) {
+        sum = sum + (i+1)
+        //console.log("i = "+i)
+        //console.log("sum = "+sum)
+    }
+    return sum
 };
 
 // Do not edit below this line
