dd52e0f2671cbde229e871719bd36d0021595bff
Ciber <cibermonster@gmail.com>
2022-07-13 15:05:07 -0400

    Remove All

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -6,15 +6,13 @@ const removeFromArray = function() {
     var remarr = arguments
     const len = arguments.length
     console.log("Before: "+arr)
-    console.log("Test: "+remarr)
     console.log("toRemove: "+len)
     
-    for (i = 1; i <= len-1; i++) {  
-        if (arr.indexOf(arguments[i]) > 0){
+    for (i = 0; i <= len+1; i++) {
+        if (arr.indexOf(arguments[i]) != '-1') {
+            console.log("Removed: "+arguments[i])
             arr.splice(arr.indexOf(arguments[i]), 1)
         }
-        //console.log("REMOVE!: "+arr.indexOf(arguments[i]))
-        
     }
 
     console.log("After: "+arr)
