941fa19bfb956233f70a307331be073eeb8f4ef5
Ciber <cibermonster@gmail.com>
2022-07-13 11:26:52 -0400

    Progressing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, args) {
+    var arr = array
+    for (i = arguments.length - 1; i > 0; i --) {
+        
+        console.log(arr)
+    }
+    return arr
 };
 
 // Do not edit below this line
