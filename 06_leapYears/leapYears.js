ec1d294e5bb21050b7e7c0521f2ae57b02646d15
Ciber <cibermonster@gmail.com>
2022-07-14 10:46:07 -0400

    Passing 4

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,8 @@
 const leapYears = function(y) {
     var x = y / 4
     var x2 = x - Math.floor(x)
-    if (x2 !== 0) {
+    var endswithzero = y % 10
+    if (x2 !== 0 || endswithzero == 0) {
         return false
     } 
     return true
