227ca6e8649c53352400bdd2b4a256c7613f521f
Ciber <cibermonster@gmail.com>
2022-07-14 13:01:15 -0400

    Working on ctof

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const ftoc = function(f) {
-  var result = 0
-  result =(f - 32) * (5/9)
+  var result = (f - 32) * (5/9)
   return parseFloat(result.toPrecision(3))
 };
 
 const ctof = function(c) {
-
+  var result = c * (9/5) + 32
+  return parseFloat(result.toPrecision(3))
 };
 
 // Do not edit below this line
