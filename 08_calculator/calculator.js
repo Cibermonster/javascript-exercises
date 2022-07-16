a596254bf67d912c312b5e9609f05c99538aa7cb
Cibermonster <cibermonster@gmail.com>
2022-07-16 18:39:27 -0400

    Complete Sum Array -Updated

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -8,9 +8,9 @@ const subtract = function(x,y) {
 
 const sum = function(arr) {
   var sum = 0
-  for (var i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
+  arr.forEach(function(n) {
+    sum += n;
+  });
 	return sum;
 };
 
