358431297479b1408445058f02082c886bfd1ac8
Ciber <cibermonster@gmail.com>
2022-07-18 13:31:38 -0400

    Palindrome complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const before = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    var newString = "";
+    for (var i = before.length - 1; i >= 0; i--) {
+        newString += before[i];
+    }
+    if (before == newString) {
+        return true
+    }
+    return false
 };
 
 // Do not edit below this line
