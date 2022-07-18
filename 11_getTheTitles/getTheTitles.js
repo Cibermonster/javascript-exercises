a23d9df4e124561d09c4156865b4641d95d34a10
Ciber <cibermonster@gmail.com>
2022-07-18 16:30:38 -0400

    GetTheTitles complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    var titles = []
+    arr.forEach(x => { titles.push(x.title); });
+    return titles;
 };
 
 // Do not edit below this line
