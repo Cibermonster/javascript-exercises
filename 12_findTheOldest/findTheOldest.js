64c0138c45bdce636b1bba2ad156a4dedd66a960
Ciber <cibermonster@gmail.com>
2022-07-19 10:40:41 -0400

    findTheOldest Complete + Ternary

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,14 +1,15 @@
 const findTheOldest = function(people) {
     people.sort(function(a,b) { 
-        aAge = a.yearOfDeath - a.yearOfBirth
-        bAge = b.yearOfDeath - b.yearOfBirth
-        if (aAge < bAge) {
+        b.yearOfDeath ??= new Date().getFullYear()
+        let aAge = a.yearOfDeath - a.yearOfBirth
+        let bAge = b.yearOfDeath - b.yearOfBirth
+        return x = aAge < bAge ? 1 : -1
+        /*if (aAge < bAge) {
             return 1;
         } else {
             return -1;
-        }
+        }*/
     });
-    console.lod(Date().getFullYear());
     return people[0];
 };
 
